d3bc0af3670c4e096072c6882d0c08427ebf38c7
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-06-05 20:55:31 +0500

    Added a todo renderer and a DOMHandler function factory.

--- src/main.js
+++ src/main.js
@@ -1,31 +1,69 @@
-const folder = function(name) {
-    const todos = [];
-    const addTodo = function(todo) {
-        todos.push(todo);
+const DOMhandler = function (parentID) {
+    const parentContainer = document.getElementById(parentID);
+    const renderFolder = function (folder) {
+
     }
+    const renderTodosInFolder = function (folder) {
+        
+    }
+    const renderTodo = function (todo) {
+        const todoContainer = document.createElement("div");
+        todoContainer.id = todo.id;
+
+        const title = document.createElement("h1");
+        title.textContent = todo.title;
+
+        const description = document.createElement("p");
+        description.textContent = todo.description;
+
+        const todoProperties = [title, description];
+        todoProperties.forEach(property => {
+            todoContainer.appendChild(property);
+        });
+
+        return todoContainer;
+    }
+    return{
+        renderTodo,
+    }
+}
+
+const folder = function (name) {
+    const todos = [];
+    const addTodo = function (todo) {
+        todos.push(todo);
+    };
 
     return {
+        id: crypto.randomUUID(),
         name,
         todos,
-        addTodo
-    }
-}
+        addTodo,
+    };
+};
 
-const todo = function(title, description) {
+const todo = function (title, description) {
     return {
+        id: crypto.randomUUID(),
         title,
-        description
-    }
-}
+        description,
+    };
+};
 
-const washDishes = todo("Wash the Dishes");
-console.log(washDishes);
 
-const fixTheKitchenSink = todo("Fix kitchen sink", "Wife will kill me if I don't fix it today!");
+(function () {
+    const washDishes = todo("Wash the Dishes");
+    console.log(washDishes);
 
-const houseChores = folder("House Chores");
-houseChores.addTodo(washDishes);
-houseChores.addTodo(fixTheKitchenSink);
+    const fixTheKitchenSink = todo("Fix kitchen sink", "Wife will kill me if I don't fix it today!");
 
-console.log(houseChores.todos);
+    const houseChores = folder("House Chores");
+    houseChores.addTodo(washDishes);
+    houseChores.addTodo(fixTheKitchenSink);
 
+    console.log(houseChores.todos);
+    const renderer = DOMhandler(document.body);
+    const washDishesRender = renderer.renderTodo(fixTheKitchenSink);
+
+    document.body.appendChild(washDishesRender);
+})();
